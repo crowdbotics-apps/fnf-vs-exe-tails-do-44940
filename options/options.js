b549b82e32c7b2e620a7cae2bfee5f7c44334d20
Crowdbotics <team@crowdbotics.com>
2023-12-10 18:55:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fnf-vs-exe-tails-do-44940",
+  url: "https://fnf-vs-exe-tails-do-44940.botics.co",
+  api: "https://fnf-vs-exe-tails-do-44940.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
